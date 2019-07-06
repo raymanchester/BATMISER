ca132beecab6a1035a47fd92858e8d98e583de68
Iowaska <51739661+IOWASKA@users.noreply.github.com>
2019-07-06 13:37:11 +0100

    Update Run Miser.bat

--- Run Miser.bat
+++ Run Miser.bat
@@ -1,31 +1,135 @@
 @ECHO off
-COLOR 06
 TITLE BATMISER 0.1 BETA [IOWASKA]
-ECHO ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-ECHO BATMISER 0.1 BETA
-ECHO ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-ECHO Miser by IOWASKA - The Businessman's Trip
-ECHO ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+COLOR 06
+ECHO +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ECHO                               BATMISER 0.1 BETA                  
+ECHO +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ECHO                    Miser by IOWASKA - The Businessman's Trip     
+ECHO +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ECHO 1. Install Brave Browser and Make Default in Windows 10
 ECHO 2. Enable Brave Rewards
 ECHO 3. Select 5 Ads Per Hour
-ECHO 4. Back Up BAT Wallet (In Brave)
-ECHO 5. Untick Auto-Contribute In Brave Settings
-ECHO 6. Close Brave Browser
-ECHO 7. Download BATMISER GitHub Repo as .ZIP: https://github.com/IOWASKA/BATMISER/
-ECHO 8. Extract into new folder
-ECHO 9. Double click Run Miser.bat
-ECHO ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-ECHO ! Donate BAT to Iowaska: 0x5000ABf72C52F0C70f2Df0E0498e61B67ec3F6fF !
-ECHO ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ECHO 4. Back Up BAT Wallet in Brave rewards settings
+ECHO 5. Untick Auto-Contribute In Brave rewards Settings
+ECHO 6. Select brave to open a new tab when it starts in brave settings
+ECHO 7. Close Brave Browser
+ECHO 8. Download BATMISER GitHub Repo as .ZIP: https://github.com/IOWASKA/BATMISER/
+ECHO 9. Extract into new folder
+ECHO 10. Double click Run Miser.bat
+ECHO +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ECHO       Donate BAT to Iowaska: 0x5000ABf72C52F0C70f2Df0E0498e61B67ec3F6fF
+ECHO +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
 
 :loop1
-ECHO Opening Brave Now - Please leave your mouse hovering over Brave
+
+ECHO Opening coinmarketcap.com - Please leave your mouse hovering over Brave
+START brave.exe "https://www.coinmarketcap.com/currencies/basic-attention-token"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://www.github.com - Please leave your mouse hovering over Brave
+START brave.exe "https://www.github.com/"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://www.ebay.com - Please leave your mouse hovering over Brave
+START brave.exe "https://www.ebay.com/"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://www.youtube.com - Please leave your mouse hovering over Brave
+START brave.exe "https://www.youtube.com/"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://www.ft.com - Please leave your mouse hovering over Brave
+START brave.exe "https://www.ft.com"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening batmiser.html - Please leave your mouse hovering over Brave
 START BATMISER.html
-TIMEOUT 301 /NOBREAK
+TIMEOUT 50 /NOBREAK
 ECHO Preparing to Scroll down now
 TIMEOUT 4 /NOBREAK
 CALL mouse.bat scrollDown 10000
@@ -36,18 +140,21 @@ CALL mouse click
 ECHO Mouse Click Complete
 ECHO Preparing to scroll up
 TIMEOUT 4 /NOBREAK
-CALL mouse.bat scrollUp 1000
+CALL mouse.bat scrollUp 10000
 ECHO Scroll Up Complete
 ECHO Preparing to click left mouse button
 TIMEOUT 4 /NOBREAK
 CALL mouse click
 ECHO Mouse Click Complete
-ECHO Preparing to scroll down
-TIMEOUT 4 /NOBREAK
-CALL mouse.bat scrollDown 10000
-ECHO Scroll Down Complete
-TIMEOUT 4 /NOBREAK
-TIMEOUT 301 /NOBREAK
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+TIMEOUT 30 /NOBREAK
 ECHO Preparing to Scroll down now
 TIMEOUT 4 /NOBREAK
 CALL mouse.bat scrollDown 10000
@@ -58,18 +165,124 @@ CALL mouse click
 ECHO Mouse Click Complete
 ECHO Preparing to scroll up
 TIMEOUT 4 /NOBREAK
-CALL mouse.bat scrollUp 1000
+CALL mouse.bat scrollUp 10000
 ECHO Scroll Up Complete
 ECHO Preparing to click left mouse button
 TIMEOUT 4 /NOBREAK
 CALL mouse click
 ECHO Mouse Click Complete
-ECHO Preparing to scroll down
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+
+ECHO Opening https://ethereumworldnews.com - Please leave your mouse hovering over Brave
+START brave.exe "https://ethereumworldnews.com"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
 TIMEOUT 4 /NOBREAK
 CALL mouse.bat scrollDown 10000
 ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
 TIMEOUT 4 /NOBREAK
-TIMEOUT 301 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://www.crypto-news.net - Please leave your mouse hovering over Brave
+START brave.exe "https://www.crypto-news.net"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://cryptonews.com - Please leave your mouse hovering over Brave
+START brave.exe "https://cryptonews.com"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://cryptocurrencynews.com - Please leave your mouse hovering over Brave
+START brave.exe "https://cryptocurrencynews.com"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening https://www.ccn.com - Please leave your mouse hovering over Brave
+START brave.exe "https://www.ccn.com"
+TIMEOUT 50 /NOBREAK
+ECHO Preparing to Scroll down now
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollDown 10000
+ECHO Scroll Down Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+
+ECHO Opening batmiser.html - Please leave your mouse hovering over Brave
+START BATMISER.html
+TIMEOUT 50 /NOBREAK
 ECHO Preparing to Scroll down now
 TIMEOUT 4 /NOBREAK
 CALL mouse.bat scrollDown 10000
@@ -80,26 +293,53 @@ CALL mouse click
 ECHO Mouse Click Complete
 ECHO Preparing to scroll up
 TIMEOUT 4 /NOBREAK
-CALL mouse.bat scrollUp 1000
+CALL mouse.bat scrollUp 10000
 ECHO Scroll Up Complete
 ECHO Preparing to click left mouse button
 TIMEOUT 4 /NOBREAK
 CALL mouse click
 ECHO Mouse Click Complete
-ECHO Preparing to scroll down
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+TIMEOUT 30 /NOBREAK
+ECHO Preparing to Scroll down now
 TIMEOUT 4 /NOBREAK
 CALL mouse.bat scrollDown 10000
 ECHO Scroll Down Complete
+ECHO Preparing to click left mouse button
 TIMEOUT 4 /NOBREAK
+CALL mouse click
+ECHO Mouse Click Complete
+ECHO Preparing to scroll up
+TIMEOUT 4 /NOBREAK
+CALL mouse.bat scrollUp 10000
+ECHO Scroll Up Complete
+ECHO Preparing to click left mouse button
+TIMEOUT 4 /NOBREAK
+CALL mouse click
+ECHO Mouse Click Complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy 10x11
+ECHO Mouse wiggle complete
+ECHO Preparing to wiggle your mouse
+TIMEOUT 4 /nobreak
+call mouse moveBy -10x-11
+ECHO Mouse wiggle complete
+
 ECHO Preparing to restart brave
 TIMEOUT 4 /NOBREAK
 taskkill /f /im brave.exe
-ECHO Preparing to restart Brave
-TIMEOUT 4 /NOBREAK
 GOTO loop1
 
 
 
- 
+
 
 
